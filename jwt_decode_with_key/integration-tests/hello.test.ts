e84ece8b3bab3a1bff1c4b79bc741a84824347b2
ccppoo <shmishmi79@gmail.com>
2023-07-01 17:58:41 +0900

    update test code

--- jwt_decode_with_key/integration-tests/hello.test.ts
+++ jwt_decode_with_key/integration-tests/hello.test.ts
@@ -1,11 +1,27 @@
-import assert from 'assert/strict'
-import { test } from 'mocha'
-import './hooks'
+import assert from "assert/strict";
+import { test } from "mocha";
+import "./hooks";
 
+const SAMPLE_JWT_SUCCESS =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.RsW4fMl-kplkQh-Y-fEMm8wNDutQtXXSOO-SGG4biJ4";
 
-test('/hello?name=njs', async function () {
-  const resp = await this.client.get('hello?name=njs')
+const SAMPLE_JWT_FAIL =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
-  assert.equal(resp.statusCode, 200)
-  assert.match(resp.body, /Meow, njs!/)
-})
+test("With wrong JWT sign", async function () {
+  const resp = await this.client.get("/", {
+    headers: { Authorization: SAMPLE_JWT_FAIL },
+  });
+
+  assert.equal(resp.statusCode, 200);
+  assert.match(resp.body, /not allowed/);
+});
+
+test("With right JWT sign", async function () {
+  const resp = await this.client.get("/", {
+    headers: { Authorization: SAMPLE_JWT_SUCCESS },
+  });
+
+  assert.equal(resp.statusCode, 200);
+  assert.match(resp.body, /Hello John Doe/);
+});
